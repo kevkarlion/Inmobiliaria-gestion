c965559aed4c19493e225207df8b5392585719bf
Kevin Riquelme <kriquelme10@gmail.com>
2026-02-03 15:11:32 -0300

    fix: sizes text in hero

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -6,30 +6,28 @@ export default function Hero() {
     <section className="relative w-full min-h-[90vh] flex items-center justify-center overflow-hidden">
       {/* Background & Overlay */}
       <div className="absolute inset-0 z-0">
-        {/* LA IMAGEN VA AQUÍ */}
         <Image
-          src="/bg-hero.png" // Ruta de tu imagen en la carpeta /public
+          src="/bg-hero.png"
           alt="Inmobiliaria Riquelme"
           fill
           className="object-cover"
-          priority // Carga la imagen con prioridad por ser el Hero
+          priority
         />
-        
-        {/* Overlay elegante: mezcla de Oxford Blue y negro para contraste */}
-       <div className="absolute inset-0 bg-black/20" />
+        <div className="absolute inset-0 bg-black/20" />
       </div>
 
       {/* Content */}
-      <div className="relative bottom-25 z-10 w-full max-w-7xl mx-auto px-6 py-20 flex flex-col items-center">
-        <div className="text-center mb-12 space-y-4">
-          <h1 className="text-white">
+      {/* Ajustado el bottom y el padding para mejor armonía en laptops */}
+      <div className="relative md:bottom-10 lg:bottom-20 z-10 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center">
+        <div className="text-center mb-8 lg:mb-12 space-y-4">
+          <h1 className="text-white text-4xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-montserrat tracking-tight leading-tight">
             Estrategia para vender, <br />
             <span className="text-gold-sand">visión para comprar.</span>
           </h1>
         </div>
 
-        {/* Buscador */}
-        <div className="w-full max-w-5xl">
+        {/* Buscador: Reducido el max-width en laptops para que no sea tan ancho */}
+        <div className="w-full max-w-md md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
           <SearchBar />
         </div>
       </div>
