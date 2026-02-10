ef8cb847ee52c433dfc544d6ad776a9186c943f7
Kevin Riquelme <kriquelme10@gmail.com>
2026-02-10 15:48:17 -0300

    working in hero v6

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -6,14 +6,14 @@ export default async function Hero() {
 
   return (
     <section
-      className="relative w-full min-h-screen xl:min-h-[110vh] flex items-center justify-center overflow-hidden
+      className="relative w-full min-h-screen xl:min-h-[110vh] flex items-center justify-center
                  bg-[url('/hero-mobile.webp')] lg:bg-[url('/bg-hero.webp')] bg-cover bg-center"
     >
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/40" />
 
       {/* Contenido */}
-      <div className="relative z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center mt-16">
+      <div className="relative z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center">
         <div className="text-center mb-8 lg:mb-12 space-y-4">
           <h1 className="text-white text-4xl xl:text-5xl font-montserrat uppercase font-black italic drop-shadow-md">
             Estrategia para vender, <br />
