40eb98d857a37a22fb90c178dc4359cd3fb1f510
Kevin Riquelme <kriquelme10@gmail.com>
2026-02-10 14:37:25 -0300

    working in hero v2

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,53 +1,34 @@
 import SearchBar from "@/components/shared/SearchBar/SearchBar";
 import { getUiProperties } from "@/components/server/data-access/get-ui-properties";
-import Image from "next/image";
 
 export default async function Hero() {
   const allProperties = await getUiProperties({ limit: 20 });
 
   return (
-    <section className="relative lg:top-25 w-full h-[95vh] flex items-center justify-center overflow-hidden">
-      
-      {/* Imagen responsive: mobile y desktop */}
-      <div className="absolute inset-0 -z-10">
-        {/* Mobile */}
-        <div className="block lg:hidden w-full h-full relative ">
-          <Image
-            src="/hero-mobile.webp"
-            alt="Hero Mobile"
-            fill
-            className="object-cover"
-            priority
-          />
-          <div className="absolute inset-0 bg-black/40" /> {/* Overlay */}
-        </div>
+    <section
+  className="relative lg:top-15 w-full h-[95vh] xl:h-[110vh] flex items-center justify-center overflow-hidden"
+  style={{ backgroundImage: "url('/hero-mobile.webp')" }}
+>
+  {/* Fondo desktop */}
+  <div className="hidden lg:block absolute inset-0 bg-[url('/bg-hero.webp')] bg-cover bg-center" />
 
-        {/* Desktop */}
-        <div className="hidden lg:block w-full h-full relative">
-          <Image
-            src="/bg-hero.webp"
-            alt="Hero Desktop"
-            fill
-            className="object-cover"
-            priority
-          />
-          <div className="absolute inset-0 bg-black/40" /> {/* Overlay */}
-        </div>
-      </div>
+  {/* Overlay */}
+  <div className="absolute inset-0 bg-black/40" />
 
-      {/* Contenido */}
-      <div className="relative z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center mb-16">
-        <div className="text-center mb-8 lg:mb-12 space-y-4">
-          <h1 className="text-white text-4xl xl:text-5xl font-montserrat uppercase font-black italic drop-shadow-md">
-            Estrategia para vender, <br />
-            <span className="text-gold-sand">visión para comprar.</span>
-          </h1>
-        </div>
+  {/* Contenido */}
+  <div className="relative z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center mb-16">
+    <div className="text-center mb-8 lg:mb-12 space-y-4">
+      <h1 className="text-white text-4xl xl:text-5xl font-montserrat uppercase font-black italic drop-shadow-md">
+        Estrategia para vender, <br />
+        <span className="text-gold-sand">visión para comprar.</span>
+      </h1>
+    </div>
 
-        <div className="w-full max-w-md md:max-w-3xl lg:max-w-5xl">
-          <SearchBar initialProperties={allProperties} />
-        </div>
-      </div>
-    </section>
+    <div className="w-full max-w-md md:max-w-3xl lg:max-w-5xl">
+      <SearchBar initialProperties={allProperties} />
+    </div>
+  </div>
+</section>
+
   );
 }
