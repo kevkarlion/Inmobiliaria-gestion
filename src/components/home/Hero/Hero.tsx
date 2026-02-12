234e7359899772cdad0c1109de3ed185d6c98863
Kevin Riquelme <kriquelme10@gmail.com>
2026-02-12 12:06:50 -0300

    testing hero v1

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -6,14 +6,11 @@ export default async function Hero() {
   const allProperties = await getUiProperties({ limit: 20 });
 
   return (
-    <section className="relative w-full min-h-screen flex items-center justify-center overflow-hidden">
+    <section className="relative w-full min-h-dvh flex items-center justify-center overflow-hidden">
       
-      {/* --- IMAGEN DE FONDO CON OVERLAY EXTENDIDO --- */}
-      {/* Usamos z-0 para que esté sobre el fondo del body pero bajo el texto */}
+      {/* --- IMAGEN DE FONDO EXTENDIDA (ANTI OVERSCROLL BLANCO) --- */}
       <div className="absolute inset-0 z-0">
-        
-        {/* Contenedor de imagen más alto que la sección para cubrir el overscroll */}
-        <div className="relative w-full h-[115%] -top-[10%] lg:h-full lg:top-0">
+        <div className="relative w-full h-[120%] -top-[10%]">
           
           {/* Mobile */}
           <div className="relative w-full h-full block lg:hidden">
@@ -44,8 +41,7 @@ export default async function Hero() {
       </div>
 
       {/* --- CONTENIDO DEL HERO --- */}
-      {/* Subimos el z-index para que el SearchBar sea clickable sobre la imagen */}
-      <div className="relative lg:mt-24 z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center text-center">
+      <div className="relative z-20 w-full max-w-7xl mx-auto px-6 py-10 flex flex-col items-center text-center lg:mt-24">
         <h1 className="text-white text-4xl xl:text-5xl font-montserrat uppercase font-black italic drop-shadow-md mb-8">
           Estrategia para vender, <br />
           <span className="text-gold-sand">visión para comprar.</span>
