34d9b0c7dda0a3819a30dcb96edf17be2b142565
Kevin Riquelme <kriquelme10@gmail.com>
2026-03-24 10:50:48 -0300

    delete icons novedades notas

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -110,7 +110,7 @@ export default async function PublicLayout({
   }
 
   return (
-    <div className={`${montserrat.variable} ${lora.variable} ${inter.variable}`}>
+    <div className={`${montserrat.variable} ${lora.variable} ${inter.variable} min-h-screen`}>
       <PublicBackground />
       <PropertyProvider>
         <Navbar menuStructure={menuStructure} />
@@ -118,7 +118,7 @@ export default async function PublicLayout({
           <BlogScrollRestoration />
         </Suspense>
 
-        <main className="flex flex-col min-h-screen lg:bg-white">
+        <main className="flex flex-col min-h-screen w-full md:bg-white">
           {children}
         </main>
 
